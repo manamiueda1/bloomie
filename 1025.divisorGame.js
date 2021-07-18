4a6c53f2b767af4c0594772b66c51caaaace2515
Manami Ueda <manamiueda1@gmail.com>
2021-07-18 18:47:53 -0400

    added dymanic programming solution

--- 1025.divisorGame.js
+++ 1025.divisorGame.js
@@ -7,3 +7,16 @@ var divisorGame = function(n) {
     } 
     return false
 };
+
+var divisorGame = function(N) {
+    const dp = Array(N+1).fill(false);
+  for(let i = 2; i <= N; i++) {
+      for(let j = 1; j < i; i++) {
+          if(i % j === 0 && !dp[i - j]) {
+              dp[i] = true;
+              break;
+          }
+      }
+  }
+  return dp[N]
+};
