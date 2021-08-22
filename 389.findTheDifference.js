68fb838a6bac61570f5435517b2fcb036427bd36
Manami Ueda <manamiueda1@gmail.com>
2021-08-22 17:16:35 -0400

    added findTheDifference

---
+++ 389.findTheDifference.js
@@ -0,0 +1,14 @@
+// 389. Find the Difference
+// https://leetcode.com/problems/find-the-difference/
+
+var findTheDifference = function(s, t) {
+    s = s.split('').sort()
+    t = t.split('').sort()
+    
+    for (let i = 0; i < s.length; i++){
+        if(s[i]!= t[i])
+            return t[i]
+    }
+    return t[t.length-1]
+
+};
