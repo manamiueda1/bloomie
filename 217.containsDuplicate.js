3a998238d9f06380c3a6f915909a434bb46ab58e
Manami Ueda <manamiueda1@gmail.com>
2021-08-22 21:28:55 -0400

    solved containsDuplicates

---
+++ 217.containsDuplicate.js
@@ -0,0 +1,14 @@
+// 217. Contains Duplicate
+// https://leetcode.com/problems/contains-duplicate/
+
+var containsDuplicate = function(nums) {
+    var obj = {};
+    
+    for(var i = 0; i < nums.length; i++){
+        obj[nums[i]] = obj[nums[i]] + 1 || 1;
+        
+        if(obj[nums[i]] > 1) return true;
+    }
+    
+    return false;
+};
